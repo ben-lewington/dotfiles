3186a9bb0497dc6dcbad0975a159bc93f9907318
Ben Lewington <ben.lewington91@ntlworld.com>
2024-01-03 00:40:22 +0000

    moved to preferred new structure, updated

--- dev/git/vim_plugins.fish
+++
@@ -1,8 +0,0 @@
-function list_plugin_submodules --description "list the git submodules"
-  set -l root_git_dir (git rev-parse --show-toplevel)
-  for folder in $root_git_dir/src/.local/share/nvim/site/pack/packer/{opt, start}
-    ls -d $folder/* 2> /dev/null
-  end
-end
-
-
